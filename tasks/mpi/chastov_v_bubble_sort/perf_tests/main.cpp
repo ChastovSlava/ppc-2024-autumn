5a5cb3fcfde751e348edc7fe932aadb6e1751e69
Z ECNFK <crychs142@gmail.com>
2024-12-01 21:14:30 +0300

    fix4

--- tasks/mpi/chastov_v_bubble_sort/perf_tests/main.cpp
+++ tasks/mpi/chastov_v_bubble_sort/perf_tests/main.cpp
@@ -104,14 +104,3 @@ TEST(chastov_v_bubble_sort, test_task_run) {
     ASSERT_EQ(count, 0);
   }
 }
-
-int main(int argc, char** argv) {
-  boost::mpi::environment env(argc, argv);
-  boost::mpi::communicator world;
-  ::testing::InitGoogleTest(&argc, argv);
-  ::testing::TestEventListeners& listeners = ::testing::UnitTest::GetInstance()->listeners();
-  if (world.rank() != 0) {
-    delete listeners.Release(listeners.default_result_printer());
-  }
-  return RUN_ALL_TESTS();
-}
